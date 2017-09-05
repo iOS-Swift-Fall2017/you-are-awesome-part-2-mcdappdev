cef7c600e56fffd868efcbdabaefb0e399050bdf
Jimmy McDermott <mcdappdev@gmail.com>
2017-09-05 12:20:01 -0400

    Week 2

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,16 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Variables
+    var index = 0
+    
     //MARK: - Outlets
-    @IBOutlet private weak var awesomeLabel: UILabel!
-
+    @IBOutlet private weak var messageLabel: UILabel!
+    
     //MARK: - Functions
     @IBAction private func messageButtonPressed(_ sender: UIButton) {
-        awesomeLabel.text = "You Are Awesome!"
-    }
-    
-    @IBAction private func anotherMessageButtonPressed(_ sender: UIButton) {
-        awesomeLabel.text = "You Are Great!"
+
+        let messages = ["You Are Fantastic!",
+                        "You Are Great!",
+                        "You are Amazing!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You Brighten My Day!",
+                        "You Are Da Bomb!",
+                        "I can't wait to use your app!"]
+        
+        var newIndex = -1
+        
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        } while index == newIndex
+        
+        index = newIndex
+        messageLabel.text = messages[newIndex]
+        
+        
+//        let randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+//        messageLabel.text = messages[randomIndex]
+//
+        
+//        messageLabel.text = messages[index]
+//        index = index + 1
+//
+//        if index == messages.count {
+//            index = 0
+//        }
+//
+        
+//        let message1 = "You Da Bomb!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
